5acd2fed4797b36ee8eb166e24ef622ce701bc95
dotrider <56666461+dotrider@users.noreply.github.com>
2020-09-23 11:13:26 -0700

    auth page set/setting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+//Components imports
 import { AppComponent } from './app.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { HeaderComponent } from './components/header/header.component';
+
+//Material imports
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SearchComponent } from './components/search/search.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { DisplayComponent } from './components/display/display.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    HeaderComponent,
+    SearchComponent,
+    MoviesComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +37,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
     
   ],
   providers: [],
